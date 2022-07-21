a7d8c3af81c42fc437aa74933e0c39b8e4394efe
Rudi Heydra <rudi.heydra@gmail.com>
2022-07-21 13:21:16 +1000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello World! from devops with AWS!"));
+app.get("/", (req, res) =>
+  res.send(
+    "Hello World from devops with AWS! This is a sample application deployed to AWS Elastic Beanstalk"
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
